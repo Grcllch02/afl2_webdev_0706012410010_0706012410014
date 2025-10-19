be0e46446ae46f2e8a7892d67833bfcd6f689354
Grcllch02 <gchelsea01@student.ciputra.ac.id>
2025-10-20 01:12:59 +0700

    user & produtcs

--- database/migrations/2025_10_19_174551_create_products_table.php
+++ database/migrations/2025_10_19_174551_create_products_table.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
-            $table->string('password');
-            $table->string('phone');
-            $table->string('address');
+            $table->id('category_id');
+            $table->string('price');
+            $table->string('stock_quantity');
+            $table->string('img_url');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
