e9cb16a74d4aa08826e763d670e987934b1ca680
michellempi <mmercyphe@student.ciputra.ac.id>
2025-10-20 01:27:40 +0700

    carts n revisi products

--- database/migrations/2025_10_19_182326_create_carts_table.php
+++ database/migrations/2025_10_19_182326_create_carts_table.php
@@ -11,13 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('carts', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->id('category_id')->constrained()->onDelete('cascade');
-            $table->string('price');
-            $table->string('stock_quantity');
-            $table->string('img_url')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
@@ -27,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('carts');
     }
 };
