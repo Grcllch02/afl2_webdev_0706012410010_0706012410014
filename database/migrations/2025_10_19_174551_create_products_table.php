a49605342a40603b68f87b6e780232c5d956934d
michellempi <mmercyphe@student.ciputra.ac.id>
2025-10-20 01:48:32 +0700

    model n database

--- database/migrations/2025_10_19_174551_create_products_table.php
+++ database/migrations/2025_10_19_174551_create_products_table.php
@@ -14,11 +14,18 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->id('category_id')->constrained()->onDelete('cascade');
+             // Define the category_id column first
+            $table->unsignedBigInteger('category_id');
             $table->string('price');
             $table->string('stock_quantity');
             $table->string('img_url')->nullable();
             $table->timestamps();
+
+            // Then link category_id as a foreign key
+            $table->foreign('category_id')
+                  ->references('id')
+                  ->on('categories')
+                  ->onDelete('cascade');
         });
     }
 
